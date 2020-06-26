ba4d5c2c0ef4a5d1ae51925e4db209b94a5b6deb
gkgg123 <gkgg123@naver.com>
2020-06-26 16:10:22 +0900

    가입페이지 구성완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from .forms import UserCustomCreationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
 def index(request):
     return render(request,'accounts/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCustomCreationForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            return redirect('accounts:index')
+    else:
+        form = UserCustomCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request,'accounts/signup.html',context)
