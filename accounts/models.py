780d51c224725aef427772cab1f6f7d545a45727
gkgg123 <gkgg123@naver.com>
2020-06-26 15:36:41 +0900

    pillow 패키지 추가 및 Usermodel 정의

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+
+def user_profile_path(instance,filename):
+    return 'profile/{0}/{1}'.format(instance.username,filename)
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=10,verbose_name='닉네임')
+    profile = models.ImageField(upload_to=user_profile_path,default='default/default_img.jpg',verbose_name='프로필 이미지')
+    intro = models.TextField(verbose_name='한줄 소개',blank=True)
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='followings')
