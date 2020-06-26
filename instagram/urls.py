5400454f557d477ace87384cc1bc39e876d02173
gkgg123 <gkgg123@naver.com>
2020-06-26 16:13:15 +0900

    메인화면을 로그인페이지로 바꿈

--- instagram/urls.py
+++ instagram/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings  
 from django.conf.urls.static import static
+from accounts import views as mainview
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('accounts.urls'))
+    path('accounts/',include('accounts.urls')),
+    path('',mainview.login)
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
