2d6390bb33794790bb12d785693cb891a0cd92d6
gkgg123 <gkgg123@naver.com>
2020-06-29 10:24:00 +0900

    posts 관련 model 작성완료 및 migrations 완료

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='post')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='like_posts')
+
+
+class PostComment(models.Model):
+    content = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='postcomment')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='like_comments')
+
+
+def post_image_path(instance,filename):
+    return 'profile/{0}/posts/{1}'.format(instance.username,filename)
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='postimage')
+    image = models.ImageField(upload_to=post_image_path,verbose_name='포스트이미지')
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    posts = models.ManyToManyField(Post,related_name='tags')
