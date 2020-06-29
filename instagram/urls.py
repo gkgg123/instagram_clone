b1d08373bac806955cf8e437f014de7e8c1853f5
gkgg123 <gkgg123@naver.com>
2020-06-29 10:00:05 +0900

    posts urls.py 생성

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,5 +21,6 @@ from accounts import views as mainview
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
-    path('',mainview.login)
+    path('',mainview.login),
+    path('posts/',include('posts.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
