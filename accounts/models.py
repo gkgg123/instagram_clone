18b95657a4f6e4b513d7a3cbfc69a00de944cdb2
gkgg123 <gkgg123@naver.com>
2020-07-05 22:07:34 +0900

    navbar 완성 : profile image 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-
+from imagekit.models import ImageSpecField
+from imagekit.processors import Thumbnail
 # Create your models here.
 
 def user_profile_path(instance,filename):
@@ -13,3 +14,7 @@ class User(AbstractUser):
     profile = models.ImageField(upload_to=user_profile_path,default='default/default_img.jpg',verbose_name='프로필 이미지')
     intro = models.TextField(verbose_name='한줄 소개',blank=True)
     followers = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='followings')
+    profile_thumbnail = ImageSpecField(source='profile',
+                          processors=[Thumbnail(300, 300)],
+                          format='JPEG',
+                          options={'quality': 60})
